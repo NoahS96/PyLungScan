a1ac811c7ed1a3352c0b4696fef10783bc5b5d18
noah2296 <noah2296@gmail.com>
2018-03-28 23:21:32 -0500

    Fixed reader to return array

--- DicomReader.py
+++ DicomReader.py
@@ -22,20 +22,25 @@ class DicomReader:
                     filesDCM.append(os.path.join(dirName, filename))
 
 
+        # Use the first dicom file to get metadata
         RefDicom = dicom.read_file(filesDCM[0])
         ConstPixelDims = (int(RefDicom.Rows), int(RefDicom.Columns), len(filesDCM))
         ConstPixelSpacing = (float(RefDicom.PixelSpacing[0]), float(RefDicom.PixelSpacing[1]), float(RefDicom.SliceThickness))
 
+        # Setup the arrays for each dimension of the image
         x = np.arange(0.0, (ConstPixelDims[0]+1)*ConstPixelSpacing[0], ConstPixelSpacing[0])
         y = np.arange(0.0, (ConstPixelDims[1]+1)*ConstPixelSpacing[1], ConstPixelSpacing[1])
         z = np.arange(0.0, (ConstPixelDims[2]+1)*ConstPixelSpacing[2], ConstPixelSpacing[2])
 
         DicomArray = np.zeros(ConstPixelDims, dtype=RefDicom.pixel_array.dtype)
 
+        # Add the pixel data to the dicom array
         for filenameDCM in filesDCM:
             ds = dicom.read_file(filenameDCM)
             DicomArray[:, :, filesDCM.index(filenameDCM)] = ds.pixel_array
-
+        
+        return DicomArray
+        
     # show
     #   Parameters:
     #       DicomArray  -   A 3-dimensional array of .dicom data
