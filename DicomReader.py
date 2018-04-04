d8f048bcfe1060bd855ab9a2d0688a65d8efb591
noah2296 <noah2296@gmail.com>
2018-04-04 18:48:45 -0500

    Added hounsfield conversion to dicomReader

--- DicomReader.py
+++ DicomReader.py
@@ -9,52 +9,63 @@ from matplotlib import pyplot as plt
 
 class DicomReader:
 
-    x = None
-    y = None
-    z = None
-
-    def __init__(self):
-        self.x = None
-
     #   Parameters:
     #       dirPath -   A path to the directory or parent directory of the .dicom files
     #   Purpose:
-    #       Return a 3-dimensional array of the .dicom files in the provided directory
-    def readFromDir(self, dirPath):
-        filesDCM = []
+    #       Return an array of Dicom files in sequence
+    def readFromDir(dirPath):
+        slices = []
+        
+        # read each dicom file into the slices array then sort by position
         for dirName, subdirList, fileList in os.walk(dirPath):
             for filename in fileList:
                 if '.dcm' in filename.lower():
-                    filesDCM.append(os.path.join(dirName, filename))
+                    slices.append(dicom.read_file(os.path.join(dirName, filename)))
+        slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
 
+        # get the distance between slices 
+        try:
+            slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+        except:
+            slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
 
-        # Use the first dicom file to get metadata
-        RefDicom = dicom.read_file(filesDCM[0])
-        ConstPixelDims = (int(RefDicom.Rows), int(RefDicom.Columns), len(filesDCM))
-        ConstPixelSpacing = (float(RefDicom.PixelSpacing[0]), float(RefDicom.PixelSpacing[1]), float(RefDicom.SliceThickness))
+        # set the slice thickness of each slice
+        for s in slices:
+            s.SliceThickness = slice_thickness
 
-        # Setup the arrays for each dimension of the image
-        self.x = np.arange(0.0, (ConstPixelDims[0]+1)*ConstPixelSpacing[0], ConstPixelSpacing[0])
-        self.y = np.arange(0.0, (ConstPixelDims[1]+1)*ConstPixelSpacing[1], ConstPixelSpacing[1])
-        self.z = np.arange(0.0, (ConstPixelDims[2]+1)*ConstPixelSpacing[2], ConstPixelSpacing[2])
+        return slices
 
-        DicomArray = np.zeros(ConstPixelDims, dtype=RefDicom.pixel_array.dtype)
-
-        # Add the pixel data to the dicom array
-        for filenameDCM in sorted(filesDCM):
-            ds = dicom.read_file(filenameDCM)
-            DicomArray[:, :, filesDCM.index(filenameDCM)] = ds.pixel_array
-       
-        return DicomArray
-        
-    # show
+    # covertHounsfield
     #   Parameters:
-    #       DicomArray  -   A 3-dimensional array of .dicom data
+    #       slices  -   An array of dicom data from one patient
     #   Purpose:
-    #       Display the .dicom data on a pyplot graph to determine if the array is correct
-    def show(self, DicomArray):
-        plt.figure(dpi=300)
-        plt.axes().set_aspect('equal', 'datalim')
-        plt.set_cmap(plt.gray())
-        plt.pcolormesh(self.x, self.y, np.flipud(DicomArray[:, :, 67]))
-        plt.show()
+    #       Convert the pixel data to Hounsfield Units which is the standard measurement of 
+    #       radiodensity which varies by tissue type. Because CT images are usually stored with
+    #       unsinged ints, the pixels must be converted back to Hounsfield units which can have
+    #       negative values. By converting to HU we can find specific tissue types from the images.
+    def convertHounsfield(slices):
+        images = np.stack([s.pixel_array for s in slices])
+        images = images.astype(np.int16)
+
+        # pixels of value -2000 represent pixels outside of the scan. Needs to be set to 0 for normalization.
+        images[images == -2000] = 0
+
+        for slice_index in range(len(slices)):
+
+            # Get the rescale intercept from the Dicom file for conversion with a linear function. 
+            intercept = slices[slice_index].RescaleIntercept
+            slope = slices[slice_index].RescaleSlope
+
+            if slope != 1:
+
+                # Multiply as a float for precision then convert back to int
+                images[slice_index] = slope * images[slice_index].astype(np.float64)
+                images[slice_index] = images[slice_index].astype(np.int16)
+
+                images[slice_index] += np.int16(intercept)
+
+        return np.array(images, dtype=np.int16)
+
+
+        
+
